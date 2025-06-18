45c18f242974a409f34e6581dfea33022fdbee48
Cameron Brooks <brooksc3@oregonstate.edu>
2025-06-18 13:24:00 -0700

    Initial Commit

---
+++ FileOrganizer/FileOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileOrganizerApp.swift
+//  FileOrganizer
+//
+//  Created by Cameron Brooks on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FileOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FileOrganizerTests/FileOrganizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FileOrganizerTests.swift
+//  FileOrganizerTests
+//
+//  Created by Cameron Brooks on 6/18/25.
+//
+
+import Testing
+@testable import FileOrganizer
+
+struct FileOrganizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
