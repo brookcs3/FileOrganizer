208c74fd1ef1de00a5d5ec6b866a98ab1ab918a2
Cameron Brooks <brooksc3@oregonstate.edu>
2025-06-19 02:07:23 -0700

    work on test

---
+++ FileOrganizer/LanguageModelSessionProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  LanguageModelSessionProtocol.swift
+//  FileOrganizer
+//
+//  Created by Cameron Brooks on 6/19/25.
+//
+
+import Foundation
---
+++ FileOrganizer/SessionPool.swift
@@ -0,0 +1,8 @@
+//
+//  SessionPool.swift
+//  FileOrganizer
+//
+//  Created by Cameron Brooks on 6/19/25.
+//
+
+import Foundation
